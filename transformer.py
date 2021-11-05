c92e71c727db52317327240dd919f37521a9e457
Yehoon Jang <jangyh0420@gmail.com>
2021-11-05 16:52:57 +0900

    transformer not compelete

---
+++ transformer.py
@@ -0,0 +1,246 @@
+import os
+import tqdm
+import math
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+
+
+
+class CustomDataset(Dataset):
+    # 반드시 init, len, getitem 구현
+    def __init__(self, df, n_past, n_future):
+        # 빈 리스트 생성 <- 데이터 저장
+        self.X = []     # n_past 만큼의 feature 데이터
+        self.y = []     # n_future 만큼의 label 데이터
+        x_col = (df.shape[1]) - 1   # df 에서 -1번째 columns 까지 x
+        for i in range(n_past, len(df) - n_future + 1): # -1 말고 변수로
+            self.X.append(df[i - n_past:i, 0:x_col])
+            self.y.append(df[i + n_future - 1: i + n_future, x_col])
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+
+# TODO: argparse 바꾸기
+
+# Device Setting
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Initializing Device: {device}')
+
+# Data Setting
+file_path = os.path.join(os.getcwd(), "data/01000002.txt")
+total_data = pd.read_csv(file_path, sep="\t", index_col="Timestamp")
+# total_data = total_data.loc[:, ["AccZ", "Str1", "Str2", "Str3"]]
+total_data = total_data.loc[:, ["Str1", "Str2", "Str3"]]
+
+minmax_scaler = MinMaxScaler(feature_range=(-1, 1))
+minmax_scaler = minmax_scaler.fit(total_data)
+total_data_scaled = minmax_scaler.transform(total_data)
+
+minmax_scaler2 = minmax_scaler.fit(np.array(total_data.Str3[:]).reshape(-1, 1))
+total_data_scaled2 = minmax_scaler2.transform(np.array(total_data.Str3[:]).reshape(-1, 1))
+
+
+train_valid_split = int(len(total_data_scaled) * 0.3)   #argparse
+df_train = total_data_scaled[:-train_valid_split]
+df_valid = total_data_scaled[-train_valid_split:]
+# print(df_train.shape, df_valid.shape)
+
+train_data = CustomDataset(df_train, 30, 1)     #argparse
+valid_data = CustomDataset(df_valid, 30, 1)
+
+batch_size = 200     #argparse
+train_loader = DataLoader(train_data, batch_size=batch_size, drop_last=True, num_workers=8)    # drop_last = drop the last incomplete batch
+valid_loader = DataLoader(valid_data, batch_size=batch_size, drop_last=True, num_workers=8)
+
+
+# Transformer
+class Transformer(nn.Module):
+    def __init__(self, dim_embed, n_feature, n_past, n_future, hidden_size, n_layers, dropout):
+        super(Transformer, self).__init__()
+
+        self.dim_embed = dim_embed
+        self.n_feature = n_feature
+        self.n_past = n_past
+        self.n_future = n_future
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        # self.embedding = nn.Embedding(n_feature, dim_embed)
+        self.embedding = nn.Linear(n_feature, dim_embed)
+        self.pos_encoder = PositionalEncoding(dim_embed)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=dim_embed, nhead=4, dropout=dropout)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=n_layers)
+
+        self.linear_mean_1 = nn.Linear(dim_embed, hidden_size*2)
+        self.linear_mean_2 = nn.Linear(hidden_size*2, hidden_size)
+        self.linear_mean_seq = nn.Sequential(self.linear_mean_1, nn.ReLU(), self.linear_mean_2)
+
+        self.linear_log_1 = nn.Linear(dim_embed, hidden_size*2)
+        self.linear_log_2 = nn.Linear(hidden_size*2, hidden_size)
+        self.linear_log_seq = nn.Sequential(self.linear_log_1, nn.ReLU(), self.linear_log_2)
+
+        self.decoder_1 = nn.Linear(hidden_size, n_future)
+        self.decoder_2 = nn.Linear(n_past, n_future)
+
+    def encoder(self, x):
+        mask = self.generate_square_subsequent_mask(len(x)).to(device)
+        src = self.embedding(x)*math.sqrt(self.dim_embed)
+        src = self.pos_encoder(src)
+
+        out = self.encoder_layer(src)
+        out = self.transformer_encoder(out, mask)
+
+        mean = self.linear_mean_seq(out)
+        logvar = self.linear_log_seq(out)
+
+        return mean, logvar
+
+    def forward(self, x):
+        mean, logvar = self.encoder(x)
+        z = self.reparameterize(mean, logvar)
+
+        out = self.decoder_1(z).transpose(1, 2)
+        # print("1 out", out.size())
+        out = self.decoder_2(out)
+        # print("2 out", out.size())
+
+        # return z, mean, logvar, out
+        return out
+
+    def generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz))==1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def reparameterize(self, mean, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps * std + mean
+
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return x + self.pe[:x.size(0), :]
+
+model = Transformer(dim_embed=512, n_feature=2, n_past=30, n_future=1, hidden_size=128, n_layers=3, dropout=0.1)
+model.to(device)
+
+
+criterion = nn.MSELoss()
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.00001)
+
+
+def train_one_epoch(model, data_loader, criterion, optimizer, device):
+    model.train()
+    criterion.train()
+
+    train_loss = 0.0
+    total = len(data_loader)
+
+    with tqdm.tqdm(total=total) as pbar:
+        for _, (X, y) in enumerate(data_loader):
+            X = X.float().to(device)
+            y = y.float().to(device)
+            y = y.unsqueeze(1)
+            # print("y size:", y.size())
+
+            # print(model(X))
+            output = model(X)   # forward
+
+            # print("output size:", output.size())
+            loss = criterion(output, y)
+            loss_value = loss.item()
+            train_loss += loss_value
+
+            optimizer.zero_grad()   # optimizer 초기화
+            loss.backward()
+            optimizer.step()    # Gradient Descent 시작
+            pbar.update(1)
+
+    return train_loss/total
+
+@torch.no_grad()    #no autograd (backpropagation X)
+def evaluate(model, data_loader, criterion, device):
+    y_list = []
+    output_list = []
+
+    model.eval()
+    criterion.eval()
+
+    valid_loss = 0.0
+    total = len(data_loader)
+
+    with tqdm.tqdm(total=total) as pbar:
+        for _, (X, y) in enumerate(data_loader):
+            X = X.float().to(device)
+            y = y.float().to(device)
+            y = y.unsqueeze(1)
+
+            output = model(X)
+            loss = criterion(output, y)
+            loss_value = loss.item()
+            valid_loss += loss_value
+
+            y_list += y.detach().reshape(-1).tolist()
+            output_list += output.detach().reshape(-1).tolist()
+            pbar.update(1)
+
+    return valid_loss/total, y_list, output_list
+
+
+# Train
+start_epoch = 0
+epochs = 100  #argparse
+print("Start Training..")
+for epoch in range(start_epoch, epochs):
+    print(f"Epoch: {epoch}")
+    epoch_loss = train_one_epoch(model, train_loader, criterion, optimizer, device)
+    print(f"Training Loss: {epoch_loss:.5f}")
+
+    valid_loss, y_list, output_list = evaluate(model, valid_loader, criterion, device)
+    rmse = np.sqrt(valid_loss)
+    print(f"Validation Loss: {valid_loss:.5f}")
+    print(f'RMSE is {rmse:.5f}')
+
+    y_list = minmax_scaler2.inverse_transform(np.array(y_list).reshape(-1, 1)).reshape(-1)
+    output_list = minmax_scaler2.inverse_transform(np.array(output_list).reshape(-1, 1)).reshape(-1)
+
+    plt.clf()
+    plt.figure(figsize=(10, 8))
+    plt.plot(y_list)
+    plt.plot(output_list)
+    data_path = os.path.join(os.getcwd(), "data", "figure")
+    if not os.path.isdir(data_path):
+        os.mkdir(data_path)
+
+    plt.savefig(f"{data_path}/figure_{int(epoch)+1}.png")
+
+
+
+
+
+
+
